fc208c6316906a09b339a9a361559e34d88db734
Kevin Lamping <klamping@gmail.com>
2016-11-20 22:57:57 -0600

    add more login tests

--- test/login.page.js
+++ test/login.page.js
@@ -1,16 +1,38 @@
+const _ = require('lodash');
+
 class Login {
   get form () { return $('form')}
   get username () { return $('input[name="email"]')}
   get password () { return $('input[name="password"]')}
   get submit () { return $('button')}
-  get errorMsg () { return $('.errorMsg')}
-  get forgotPwd () { return $('.forgot')}
-  get register () { return $('.register-link')}
+  get errorMsg () { return $('.error.message')}
+  get forgotPwd () { return $('a[href="forgot.html"]')}
+  get register () { return $('a[href="register.html"]')}
 
   login (user, pass) {
     this.username.setValue(user);
     this.password.setValue(pass);
     this.form.submitForm();
+  }
+
+  isLoggedIn () {
+    if (_.contains(browser.getUrl(), 'logged-in.html')) {
+      throw new Error('should not be on logged-in page');
+    }
+  }
+
+  logout () {
+
+  }
+
+  hasErrorMessage (msg) {
+    this.isLoggedIn()
+    if (!this.errorMsg.isVisibleWithinViewport()) {
+      throw new Error('error message should be visible');
+    }
+    if (!_.contains(this.errorMsg.getText(), msg)) {
+      throw new Error('error message should contain ' + msg);
+    }
   }
 }
 
--- test/login.spec.js
+++ test/login.spec.js
@@ -1,14 +1,57 @@
 const expect = require('chai').expect;
 const Login = require('./login.page.js');
 
-let login = new Login();
+const login = new Login();
 
 describe('Login Page', function () {
-  it('should let you log in', function () {
-    browser.url('/');
+  let validUser = 'valid@user.com';
+  let validPass = 'fadadsfasdfad';
 
-    login.login('valid@user.com', 'asdasdfasdfasd');
+  beforeEach(function () {
+    browser.url('/');
+  })
+
+  it('should let you log in', function () {
+    login.login(validUser, validPass);
 
     expect(browser.getUrl()).to.contain('logged-in.html');
+  })
+
+  it('should error on missing username', function () {
+    login.login('', validPass);
+
+    login.hasErrorMessage('Please enter a valid e-mail');
+  });
+
+  it('should error on invalid username', function () {
+    login.login('invalid', validPass);
+
+    login.hasErrorMessage('Please enter a valid e-mail');
+  });
+
+  it('should error on unknown username', function () {
+    login.login('invalid@user.com', validPass);
+
+    login.hasErrorMessage('Your username or password is incorrect');
+  });
+
+  it('should error on missing password', function () {
+    login.login(validUser, '');
+
+    login.hasErrorMessage('Please enter your password');
+  });
+
+  it('should error on bad password', function () {
+    login.login(validUser, 'as');
+
+    login.hasErrorMessage('Your password must be at least 6 characters');
+  });
+
+  it('should have forgot password link', function () {
+    expect(login.forgotPwd.isVisibleWithinViewport()).to.be.true;
+  })
+
+  it('should have register link', function () {
+    expect(login.register.isVisibleWithinViewport()).to.be.true;
   })
 })
